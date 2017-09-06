4e7493475d3130ef46146f5897762a8e7a982d77
Gabriel Malta <gmalta44@gmail.com>
2017-09-06 19:38:47 -0300

    projeto exemplo com spring

---
+++ dsw/exemplo-spring/src/main/java/br/senac/tads4/dsw/exemplospring/ExemploSpringApplication.java
@@ -0,0 +1,12 @@
+package br.senac.tads4.dsw.exemplospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploSpringApplication.class, args);
+	}
+}
